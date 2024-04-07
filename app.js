13dcc851742fb4540591f612c1bf6063e51e136b
木村元 <kimuragen@kimuramotonoMacBook-Air.local>
2024-04-07 13:30:34 +0900

    色の修正を行いました” git add * git add * git commit -m 色の修正を行いました”

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ $("#addTodo").click(function () {
 $(document).on("change", "input[type=checkbox]", function() {
   if($(this).is(":checked")) {
     $(this).parent().css("text-decoration", "line-through");
+    $(this),prarent().css("color","#ccc" )
   } else {
     $(this).parent().css("text-decoration", "none");
+    $(this),prarent().css("color","#000" )
   }
 });
